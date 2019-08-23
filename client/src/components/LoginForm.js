de259429552e67c54ee9d6b10b467456d5103f7b
ditotet <ditotet@gmail.com>
2019-08-23 18:20:19 +0400

    change LoginForm with hooks

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { log } from 'util';
 import Button from '@material-ui/core/Button'
@@ -7,44 +7,45 @@ import FormControl from '@material-ui/core/FormControl'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 
-export default class LoginForm extends Component {
-  
-  state = {
-    userName: "",
-    password: "",
-    errorText: {
-      userName: "",
-      password: ""
-    }
-  }
 
-  handleChange = (e, fieldName) => {
-    this.setState({
+const LoginForm = (props) => {
+  const [state, setState] = useState({
+    userName: '',
+    password: '',
+    errorText: {
+      userName: '',
+      password: ''
+    }
+  })
+
+  const handleChange = (e, fieldName) => {
+    setState({
+      ...state,
       [fieldName]: e.target.value,
       errorText: {
-        userName: "",
-        password: ""
+        userName: '',
+        password: ''
       }
     })
-    console.log(this.state)
   }
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
     axios.post('/login', {
-      userName: this.state.userName,
-      password: this.state.password
+      userName: state.userName,
+      password: state.password
     }).then(res => {
       if (res.data.success) {
-        this.props.history.push('/posts')
+        props.history.push('/posts')
       } else  {
-        let errorText = this.state.errorText
+        let errorText = state.errorText
         if (res.data.errorCode == 0) {
-          errorText.password = `Password for username '${this.state.userName}' is incorrect`
+          errorText.password = `Password for username '${state.userName}' is incorrect`
         } else if (res.data.errorCode == 1) {
-          errorText.userName = `User '${this.state.userName}' does not exist`  
+          errorText.userName = `User '${state.userName}' does not exist`  
         }
-        this.setState({
+        setState({
+          ...state,
           errorText: errorText
         })
       }
@@ -53,41 +54,43 @@ export default class LoginForm extends Component {
     })
   }
   
-  render() {
-    const { styles } = this.props
-    return (
-      <form noValidate onSubmit={e => this.handleSubmit(e) }>
-        <TextField
-          required
-          id="user-name"
-          label="User Name"
-          value={this.state.userName}
-          onChange={ (e) => this.handleChange(e, 'userName') }
+  useEffect(() => {
+    console.log(state)
+  })
+
+  return (
+    <form noValidate onSubmit={e => handleSubmit(e) }>
+      <TextField
+        fullWidth
+        required
+        id="user-name"
+        label="User Name"
+        value={state.userName}
+        onChange={ (e) => handleChange(e, 'userName') }
+        margin="normal"
+        variant="outlined"
+        helperText={ state.errorText.userName }
+        error= { state.errorText.userName.length > 0 }
+      />
+      <TextField
+        fullWidth
+        required
+        id="password"
+        label="Password"
+        type="password"
+        value={ state.password }
+        onChange={ (e) => handleChange(e, 'password') }
           margin="normal"
           variant="outlined"
-          style={ styles.inputField }
-          helperText={ this.state.errorText.userName }
-          error= { this.state.errorText.userName.length > 0 }
-        />
-        <TextField
-          required
-          id="password"
-          label="Password"
-          type="password"
-          value={ this.state.password }
-          onChange={ (e) => this.handleChange(e, 'password') }
-            margin="normal"
-            variant="outlined"
-          style={ styles.inputField }
-          helperText={ this.state.errorText.password }
-          error= { this.state.errorText.password.length > 0 }
-        />
-        <Button style={ styles.button } 
-                fullWidth 
-                variant="contained"  
-                color="primary" 
-                type="submit">Log in</Button>
-      </form>
-    )
-  }
+        helperText={ state.errorText.password }
+        error= { state.errorText.password.length > 0 }
+      />
+      <Button  fullWidth 
+              variant="contained"  
+              color="primary" 
+              type="submit">Log in</Button>
+    </form>
+  )
 }
+
+export default LoginForm
